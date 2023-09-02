833928d811e023763ed94b568b1620d0483e405f
Matheus Henrique <131389471+CloneTz@users.noreply.github.com>
2023-09-02 12:59:12 -0300

    curso finalizado

--- alura-midi/main.js
+++ alura-midi/main.js
@@ -1,17 +1,31 @@
-function tocaSom(idElementoAudio) {
-  document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+  const elemento = document.querySelector(seletorAudio);
+
+  if (elemento && elemento.localName === "audio") {
+    elemento.play();
+  } else {
+    alert("Elemento não encontrado ou seletor inválido");
+  }
 }
 
 const listaDeTeclas = document.querySelectorAll(".tecla");
 
-let contador = 0;
+for (contador = 0; contador < listaDeTeclas.length; contador++) {
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+  const idAudio = `#som_${instrumento}`;
 
-while (contador < listaDeTeclas.length) {
-  listaDeTeclas[contador].onclick = function () {
-    tocaSom("#som_tecla_pom");
+  tecla.onclick = function () {
+    tocaSom(idAudio);
   };
 
-  contador = contador + 1;
+  tecla.onkeydown = function (evento) {
+    if (evento.code === "Space" || evento.code === "Enter") {
+      tecla.classList.add("ativa");
+    }
+  };
 
-  console.log(contador);
+  tecla.onkeyup = function () {
+    tecla.classList.remove("ativa");
+  };
 }
